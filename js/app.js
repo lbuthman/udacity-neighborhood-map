538eee326f7a50a057ce5ceb55fbb317573ba6af
lbuthman <lbuthman@gmail.com>
2017-09-08 19:39:24 -0500

    start basic observable for knockout listener

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 var map;
 var markers = [];
 
+var embed = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBygxnA7fNhpEsVlPfPrHmkNgyWRo00LS4&q=world";
+
 function initMap() {
 
 }
 
-var defaultMap = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBygxnA7fNhpEsVlPfPrHmkNgyWRo00LS4&q=world"
+var embeddedMap = {
+  defaultMap: "static/default-map.jpg",
+  src: ""
+}
+
+var viewModel = function() {
+  var self = this;
+
+  // this.location = ko.oberservable(embeddedMap);
+
+  this.setLocation = function() {
+    alert('hi');
+  }
+}
+
+ko.applyBindings(new viewModel());
